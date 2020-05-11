05d0f237b8b0954764277b5fbb0fe5d429d2ec58
claudia v <claudia4ro@yahoo.com>
2020-05-11 21:49:04 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,45 @@
-# code here!
+
+class School
+  
+  attr_reader :name, :roster, :grade
+  
+  
+  
+  def initialize (name)
+    @name = name
+    @roster = Hash.new { |hash, key| hash[key] = [] }
+    
+    
+  end
+  
+  def add_student (name, grade)
+  
+    #roster[grade]=[]
+    if @roster.has_key?(grade)
+      @roster[grade] <<  name 
+    else
+      #@roster[grade] 
+      
+      @roster [grade] << name
+    end
+    
+    
+  end
+  
+  def grade (grade)
+    all_sudents_samegrade = []
+    @roster.each do | hash_grade, students |
+      if grade = hash_grade
+        all_sudents_samegrade <<  students
+      end
+    end
+    all_sudents_samegrade.flatten
+  end
+  
+  def sort
+    
+    
+  end
+  
+  
+end
